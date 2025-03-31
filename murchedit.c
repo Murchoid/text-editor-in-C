12a7ab741e4cbf2d235270dc2330acba163b710d
Murchoid <kamaucoder@gmail.com>
2025-03-31 17:36:36 +0300

    Added a funciton to get cursor position, not finished yet

--- murchedit.c
+++ murchedit.c
@@ -4,32 +4,42 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 #define CTRL_KEY(k) ((k)& 0x1f)
 
-struct termios orig_termios;
+struct editorConfig{
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 void die(const char *s){
+
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
 	perror(s);
 	exit(1);
 }
 
 void disableRawMode() {
 
- if( tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)==-1) die("tcsetattr");
+ if( tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios)==-1) die("tcsetattr");
 
 }
 void enableRawMode() {
 
-  if(tcgetattr(STDIN_FILENO, &orig_termios)==-1) die("tcgetattr");
+  if(tcgetattr(STDIN_FILENO, &E.orig_termios)==-1) die("tcgetattr");
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON );
- raw.c_cflag |= (CS8);
- raw.c_cc[VMIN] = 0;
- raw.c_cc[VTIME] = 1;
- raw.c_oflag &=~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  raw.c_oflag &=~(OPOST);
   raw.c_lflag &= ~(ECHO |  ICANON | ISIG |  IEXTEN);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -45,6 +55,47 @@ char editorReadKey(){
 	return c;
 }
 
+void editorDrawRows(){
+	int y ;
+	for(y=0; y< E.screenrows; y++){
+		write(STDOUT_FILENO, "~\r\n",3);
+	}
+}
+
+int getCursorPosition(int *rows, int *cols){
+	if(write(STDOUT_FILENO, "\x1b[6n", 4) !=4) return -1;
+
+	printf("\r\n");
+
+	char c;
+	
+	while(read(STDIN_FILENO, &c, 1) == 1){
+		if(iscntrl(c)){
+			printf("%d\r\n", c);
+		}else{
+			printf("%d (' %c ')\r\n", c, c);
+		}
+	}
+
+	editorReadKey();
+	return -1;
+	
+}
+int getWindowSize(int *rows, int *cols){
+	struct winsize ws;
+
+	if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    		return getCursorPosition(rows, cols);
+   		
+	}else{
+		*rows = ws.ws_row;
+		*cols = ws.ws_col;
+		return 0;
+	}
+
+}
+
 
 void editorProcessKeyPressed(){
 	char c = editorReadKey();
@@ -58,13 +109,21 @@ void editorProcessKeyPressed(){
 void editorRefreshScreen(){
 	write(STDOUT_FILENO, "\x1b[2J", 4);
 	write(STDOUT_FILENO, "\x1b[H", 3);
+	
+	editorDrawRows();
+
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void initEditor(){
+	if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
 int main() {
 	
 	
 	enableRawMode();
-
+	initEditor();
  	
   	while(1){
 		editorRefreshScreen();
