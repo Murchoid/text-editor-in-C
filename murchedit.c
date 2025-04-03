7258757059e882274ff5d32e2cd29b95e70f2676
Murchoid <kamaucoder@gmail.com>
2025-04-03 22:22:16 +0300

    Added functionality to move arrow keys with wsad and arrow key

--- murchedit.c
+++ murchedit.c
@@ -12,6 +12,7 @@
 
 
 struct editorConfig{
+	int cx,cy;
 	int screenrows;
 	int screencols;
 	struct termios orig_termios;
@@ -74,7 +75,26 @@ char editorReadKey(){
 	while((nread = read(STDIN_FILENO, &c, 1) )!= 1){
 		if(nread == -1 && errno != EAGAIN) die("read");
 	}
+
+	if(c == '\x1b'){
+		char seq[3];
+		
+		if(read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+		if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+		if(seq[0] == '['){
+			switch (seq[1]){
+				case 'A': return 'w';
+				case 'B': return 's';
+				case 'C': return 'd';
+				case 'D': return 'a';
+			}	
+		}
+
+		return '\x1b';
+	}else{
 	return c;
+	}
 }
 
 void editorDrawRows(struct abuf *ab){
@@ -84,6 +104,13 @@ void editorDrawRows(struct abuf *ab){
 			char welcome[80];
 			int welcomelen = snprintf(welcome,sizeof(welcome), "Murchoid editor -- version %s", MURCH_VERSION);
 			if(welcomelen > E.screencols) welcomelen = E.screencols;
+
+			int padding = (E.screencols- welcomelen)/2;
+			if(padding){
+				abAppend(ab, "~", 1);
+				padding--;
+			}
+			while (padding--) abAppend(ab, " ", 1);
 			abAppend(ab, welcome, welcomelen);
 		}else{
 			abAppend(ab, "~", 1);
@@ -132,12 +159,36 @@ int getWindowSize(int *rows, int *cols){
 
 }
 
+void editorMoveCursor(char key) {
+  switch (key) {
+    case 'a':
+      E.cx--;
+      break;
+    case 'd':
+      E.cx++;
+      break;
+    case 'w':
+      E.cy--;
+      break;
+    case 's':
+      E.cy++;
+      break;
+  }
+}
 
 void editorProcessKeyPressed(){
 	char c = editorReadKey();
 	switch(c){
 		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+      			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+		case 'w':
+		case 's':
+		case 'a':
+		case 'd':
+			editorMoveCursor(c);
 			break;
 	}
 }
@@ -150,14 +201,20 @@ void editorRefreshScreen(){
 	
 	editorDrawRows(&ab);
 
-	abAppend(&ab, "\x1b[H", 3);
-	abAppend(&ab, "\x1b[25h", 6);
+	char buf[32];
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy+1, E.cx+1);
+	abAppend(&ab, buf, strlen(buf));
+
+	abAppend(&ab, "\x1b[?25h", 6);
 	
 	write(STDOUT_FILENO, ab.b, ab.len);
 	abFree(&ab);
 }
 
 void initEditor(){
+	E.cx=0;
+	E.cy=0;
+
 	if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
